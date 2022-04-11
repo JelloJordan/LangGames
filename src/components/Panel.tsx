981d7295653ecc8413c9e65abe1a999be766e448
Jordan Millett <55707792+JelloJordan@users.noreply.github.com>
2022-04-10 20:15:11 -0700

    Added Panel Component

---
+++ src/components/Panel.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+export default class Panel extends React.Component <{}> 
+{
+  render() 
+  {
+    return (
+        <div id="panel" className="w-full max-w-lg flex flex-col">
+
+        </div>
+    );
+  }
+}
